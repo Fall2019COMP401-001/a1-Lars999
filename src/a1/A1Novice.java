5348533840e378c1c9d5a14278a98da1d64755aa
davidlundberg <davidlundberg@davids-mbp.lan>
2019-08-28 20:56:25 -0400

    First Commit. Looks like it works.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -10,5 +10,39 @@ public class A1Novice {
 
 		// Your code follows here.
 		
+		int num = scan.nextInt();
+		String[] arr = new String[num];
+		// Double total = 0.0;
+		
+		for (int i=0; i<arr.length; i++) {
+			String f = scan.next();
+			char firstInitial = f.charAt(0);
+			String l = scan.next();
+			int shop = scan.nextInt();
+			String[] item = new String[shop];
+			Double total = 0.0;
+			for (int a=0; a<item.length; a++) {
+				int itemNumber = scan.nextInt();
+				String name = scan.next();
+				Double price = 0.0;
+				price = scan.nextDouble();
+				total += price * itemNumber;
+				
+				
+				
+			
+				 
+			
+		}
+			
+			 System.out.println(firstInitial + ". " + l + ": " + total);
+			 
+		
+			
+		
+		
+		}	
+		
+		
 	}
 }
